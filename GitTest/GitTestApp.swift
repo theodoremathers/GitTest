535608bc51b3c72c7a526d68296390d1c6d3704a
TheodoreMathers <2967022214@qq.com>
2022-03-22 10:35:17 +0800

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Jon on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
